058efef01831669dfd778d33db6cab6bcea37417
marcelosn2 <49559726+marcelosn2@users.noreply.github.com>
2021-12-01 20:36:47 -0300

    Update EP3.main.py
    adicionando plataforma inicial e implementando funcionalidade de pulo

--- EP3.main.py
+++ EP3.main.py
@@ -6,42 +6,57 @@ import sys
 import random
 from configuracoes import *
 
-img_dir = path.join(path.dirname(__file__), 'imagens')
+img_dir = path.join(path.dirname(_file_), 'imagens')
 
 class Puli(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        dir= pygame.image.load(path.join(img_dir, "right.png")).convert()
-        self.image=dir
-        self.image=pygame.transform.scale(dir, (50,50))
+    def _init_(self):
+        pygame.sprite.Sprite._init_(self)
+        self.dir= pygame.image.load(path.join(img_dir, "right.png")).convert()
+        self.image=self.dir
+        self.image=pygame.transform.scale(self.dir, (50,50))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.centerx=WIDTH/2
-        self.rect.bottom= HEIGHT -10 
+        self.rect.bottom= HEIGHT - 40 
         self.speedx=0
-        self.speedy = 0
-        self.jump = 1
-        self.radius=50
+        self.speedy = -5
+        self.isJump = False
+        self.jumpCount = 0
+        self.radius=10
+
     def update(self):
         self.rect.x += self.speedx
-        if self.jump == 0:
-            
-            self.rect.y += self.speedy
-        else :
-            self.rect.y -= (self.speedy)+1
+        
+        if self.isJump:
+            if self.jumpCount > 0:
+                self.rect.y += self.speedy
+                self.jumpCount -= 1
+            else:
+                self.rect.y -= self.speedy 
+        else:
+            self.rect.y -= self.speedy 
+
+
+        self.dir= self.dir
+        self.image=self.dir
+        self.image=pygame.transform.scale(self.dir, (50,50))
+        self.image.set_colorkey(BLACK)
+
+
 
         if self.rect.right>WIDTH:
             self.rect.left=0
         if self.rect.left<0:
             self.rect.right=WIDTH
 
+
 class PlatV(pygame.sprite.Sprite):
-    def __init__(self):
+    def _init_(self):
 
         x = random.randint(0,WIDTH)
         y = random.randint(0,HEIGHT)
 
-        pygame.sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite._init_(self)
         verde=pygame.image.load(path.join(img_dir, "green.png")).convert()
         self.image=verde
         self.image=pygame.transform.scale(verde, (120,30))
@@ -49,19 +64,37 @@ class PlatV(pygame.sprite.Sprite):
         self.width=120
         self.height=30
         self.rect=self.image.get_rect()
-        self.rect.centerx=x
+        self.rect.centerx= x
         self.rect.bottom= y
         self.speedx=0
 
+class PlatVInit(pygame.sprite.Sprite):
+    def _init_(self):
+
+        pygame.sprite.Sprite._init_(self)
+        verde=pygame.image.load(path.join(img_dir, "green.png")).convert()
+        self.image=verde
+        self.image=pygame.transform.scale(verde, (120,30))
+        self.image.set_colorkey(BLACK)
+        self.rect=self.image.get_rect()
+        self.rect.centerx=WIDTH/2
+        self.rect.bottom= HEIGHT -10 
+        self.speedx=0
+        self.width=120
+        self.height=30
+
+    def update(self):
+        self.rect.bottom = self.rect.bottom 
+        self.rect.centerx=self.rect.centerx
 
 
 class PlatB(pygame.sprite.Sprite):
-    def __init__(self):
+    def _init_(self):
         x = random.randint(0,WIDTH)
         y = random.randint(0,HEIGHT)
         
-        pygame.sprite.Sprite.__init__(self)
-        azul=pygame.image.load('imagens/blue.png').convert()
+        pygame.sprite.Sprite._init_(self)
+        azul=pygame.image.load(path.join(img_dir, "blue.png")).convert()
     
         self.image=pygame.transform.scale(azul, (120,30))
         self.image.set_colorkey(BLACK)
@@ -91,13 +124,18 @@ background_rect = background.get_rect()
 
 
 player = Puli()
-platB=PlatB()
+platvinit = pygame.sprite.Group()
+platV = pygame.sprite.Group()
+platB = pygame.sprite.Group()
+
 # Cria um grupo de todos os sprites e adiciona a nave.
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
+all_sprites.add(PlatVInit())
+platvinit.add(PlatVInit())
 
-platV = pygame.sprite.Group()
-platB = pygame.sprite.Group()
+
+
 for i in range(8):
     m = PlatV()
     all_sprites.add(m)
@@ -114,6 +152,8 @@ try:
         
         # Ajusta a velocidade do jogo.
         clock.tick(FPS)
+
+
         
         # Processa os eventos (mouse, teclado, botão, etc).
         for event in pygame.event.get():
@@ -146,10 +186,26 @@ try:
 
         hits = pygame.sprite.spritecollide(player, platV, False, pygame.sprite.collide_circle)
         if hits:
-            player.jump = 1
+            player.isJump = True
+            player.jumpCount = 30
+            player.speedy = -5
+            player.dir = pygame.image.load(path.join(img_dir, "right.png")).convert()
+
+        else:
+            player.dir = pygame.image.load(path.join(img_dir, "right_1.png")).convert()
+      
+
+        hits = pygame.sprite.spritecollide(player, platvinit, False)
+        if hits:
+            player.isJump = True
+            player.jumpCount = 30
+            player.speedy = -5
+            player.dir = pygame.image.load(path.join(img_dir, "right.png")).convert()
             
-        else :
-            player.jump = 0
+
+        else:
+            player.dir = pygame.image.load(path.join(img_dir, "right_1.png")).convert()       
+            
             
            
       
